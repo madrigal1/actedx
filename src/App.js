efc6739e7c2fca0fc601dc041e47a24f8f121ef6
madrigal1 <kurupgokul11@gmail.com>
2021-01-15 17:10:14 +0530

    feat:structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+import {useRef,useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ScrollMagic from 'scrollmagic'
 
+import {TweenMax,Power3} from 'gsap';
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(()=>{
+    // console.log(logoItem);
+    // logoItem.current.style.display ="none";
+    // TweenMax.to(logoItem.current,.8,{
+    //   opacity:1,
+    //   y:-20,
+    //   ease:Power3.easeOut,
+    // });
+    // TweenMax.to(textItem.current,.8,{
+    //   y:-60,
+    //   ease:Power3.easeOut,
+    //   delay:.2
+    // })
+    const controller = new ScrollMagic.Controller();
+    const scence = new ScrollMagic.Scene({
+      triggerElement:"#speakers"
+    });
+
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
       </header>
+      <section id="speakers">
+        <p>First</p>
+      </section>
+      <section>
+        <p>Second</p>
+      </section>
     </div>
   );
 }
