bcbbeedd029e32b171f3998ced8dfd4e6e1585c2
madrigal1 <kurupgokul11@gmail.com>
2021-01-16 13:41:48 +0530

    feat:font

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         end: "+=100%",
         pinSpacing:true
       },
-      y:-1000,
+      y:-800,
      // transformOrigin: "left center", 
       ease: "none"
     });
@@ -53,24 +53,53 @@ function App() {
         <div className="card_type1">
           <div className="card_header">
               <div className="ch_container">
-              <div className="black_circle"></div>
-              <span className="handle">@myfriendslvia</span>
+                <div className="black_circle"></div>
+                <span className="handle">@myfriendslvia</span>
               </div>
           </div>
           <div className="speaker_pic">
             <img src={speaker1} alt=""/>
           </div>
-          <div className="speaker_desc"></div>
+          <div className="speaker_info">
+            <h1 className="speaker_name">Sylvia</h1>
+            <span className="speaker_desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iure maiores quos illo quia! Architecto alias odio magnam vero aliquam!
+            </span>
+          </div>
         </div>
         <div className="card_type1">
-          <div className="card_header"></div>
-          <div className="speaker_pic"></div>
-          <div className="speaker_desc"></div>
+          <div className="card_header">
+             <div className="ch_container">
+                <div className="black_circle"></div>
+                <span className="handle">@chetanbhagat</span>
+              </div>
+          </div>
+          <div className="speaker_pic">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/37551462_205576046977748_5207059345686134784_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=hV9yF4XcJIgAX9C-3dn&tp=1&oh=4468c29937c504f387ea3fcf7e72b74f&oe=602D79FE" alt=""/>
+          </div>
+          <div className="speaker_info">
+            <h1 className="speaker_name">Chetan Bhagat</h1>
+            <span className="speaker_desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iure maiores quos illo quia! Architecto alias odio magnam vero aliquam!
+            </span>
+          </div>
         </div>
         <div className="card_type1">
-          <div className="card_header"></div>
-          <div className="speaker_pic"></div>
-          <div className="speaker_desc"></div>
+          <div className="card_header">
+          <div className="ch_container">
+                <div className="black_circle"></div>
+                <span className="handle">@rickriordan</span>
+              </div>
+          </div>
+          <div className="speaker_pic">
+            <img src="https://instagram.fcok1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/119070998_127386335746682_936085938337847283_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_ohc=Di3SbRUYd-IAX-OFlgK&tp=1&oh=d98247a1955ffd3d0a6a89b1df4360dd&oe=602A5161" alt=""/>
+          </div>
+          <div className="speaker_info">
+           <h1 className="speaker_name">Rick Riordan</h1>
+            <span className="speaker_desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iure maiores quos illo quia! Architecto alias odio magnam vero aliquam!
+            </span>
+          </div>
         </div>
     
     
